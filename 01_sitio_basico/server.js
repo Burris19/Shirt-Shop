091ee2832a7d472b2f908210589b895552bf8820
Burris19 <julianhernandez19@gmail.com>
2015-12-10 21:23:32 +0000

    [add] create server basic with requests

--- 01_sitio_basico/server.js
+++ 01_sitio_basico/server.js
@@ -2,9 +2,9 @@ var http = require('http');
 
 http.createServer(function (request, response) {    
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Hola mundo");
+    response.write("Bienvenido");
     response.end();    
     
-}).listen(3000, '192.168.10.10');
+}).listen(3000,'localhost');
 
 console.log('The server show http://192.168.10.10:3000');
